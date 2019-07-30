ab5ea4841c38118c17ba750ec3e58c43cf7a06fe
Pawel Piwosz <pawel@pawelpiwosz.net>
2019-07-30 22:21:48 +0200

    next part of pylint fixes

--- py/cloud_control_terminate_ec2.py
+++ py/cloud_control_terminate_ec2.py
@@ -15,7 +15,7 @@ def cloud_control_state_action_ec2(event, context):
         ]
         instances = ec2.instances.filter(Filters=ec2_filter)
         instances_to_terminate = [instance.id for instance in instances]
-        if len(instances_to_terminate) > 0:
+        if instances_to_terminate:
             #perform the shutdown
             ec2.instances.filter(
                 InstanceIds=instances_to_terminate).terminate()
@@ -23,26 +23,25 @@ def cloud_control_state_action_ec2(event, context):
             return {"msg": msg}
         msg = "Nothing to terminate!"
         return {"msg": msg}
-    else:
-        response = ec2_client.describe_instances(
-            Filters=[
-                {
-                    'Name': 'tag:Name',
-                    'Values': [event["body"]["InstanceName"]]
-                }
-            ]
+    response = ec2_client.describe_instances(
+        Filters=[
+            {
+                'Name': 'tag:Name',
+                'Values': [event["body"]["InstanceName"]]
+            }
+        ]
+    )
+    instance_list = []
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instance_list.append(instance['InstanceId'])
+    if not instance_list:
+        msg = "I cannot find the instance with name {}.".format(
+            event["body"]["InstanceName"]
         )
-        instance_list = []
-        for reservation in response['Reservations']:
-            for instance in reservation['Instances']:
-                instance_list.append(instance['InstanceId'])
-        if not instance_list:
-            msg = "I cannot find the instance with name {}.".format(
-                event["body"]["InstanceName"]
-            )
-            return {"msg": msg}
-        temp_msg = "I found instance {}. ".format(event["body"]["InstanceName"])
-        ec2.instances.filter(
-            InstanceIds=instance_list).terminate()
-        msg = temp_msg + "Terminating."
         return {"msg": msg}
+    temp_msg = "I found instance {}. ".format(event["body"]["InstanceName"])
+    ec2.instances.filter(
+        InstanceIds=instance_list).terminate()
+    msg = temp_msg + "Terminating."
+    return {"msg": msg}
